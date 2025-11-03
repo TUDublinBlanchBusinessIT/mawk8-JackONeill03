d1d509f732b8d3d3adcc60a82d97dec1d0eef78a
B00155573 Jack O Neill <B00155573@mytudublin.ie>
2025-11-03 16:21:47 +0000

    Step 1 - 6

--- firestoreConfig.js
+++ firestoreConfig.js
@@ -2,7 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Your Firebase configuration object goes here
+  "projectId": "wk8lab1",
+  "appId": "1:410910775372:web:d1bda46704a4380d33bce9",
+  "storageBucket": "wk8lab1.firebasestorage.app",
+  "apiKey": "AIzaSyA0V_OCmvsIf32LhKXk6uD5uIXgSnhdlCE",
+  "authDomain": "wk8lab1.firebaseapp.com",
+  "messagingSenderId": "410910775372",
+  "projectNumber": "410910775372",
+  "version": "2"
 }
 
 const app = initializeApp(firebaseConfig);
